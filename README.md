04e6f1e184ee76131c9b0daa26b0b1cb8603baf3
m6keller <kellermatthewe@gmail.com>
2022-06-12 16:55:10 -0700

    read me and fixed response formatting

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Next steps
 
 - To-Do List Feature
 - Random questions/quiz feature
+- Organize functions into different files. Refer [here](https://rust-cli.github.io/book/index.html) and [here](https://doc.rust-lang.org/book/ch07-02-defining-modules-to-control-scope-and-privacy.html)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use structopt::StructOpt; // automatically parses the arguments
 use exitfailure::{ExitFailure}; // error handling
-// use serde_derive::{Deserialize, Serialize}; // deserialize the json file
+use serde_derive::{Deserialize, Serialize}; // deserialize the json file
 use reqwest::Url; // url parsing   
 
 // use openweathermap.org/api
@@ -13,90 +13,105 @@ struct CLI {
     country_code: String
 }
 
-// #[derive(Serialize, Deserialize)]
-// struct Forecast {
-//     coord: Coord,
-//     weather: Weather,
-//     base: String,
-//     main: Temps,
-//     visibility: i32,
-//     wind: Wind,
-//     clouds: Clouds,
-//     dt: i32,
-//     sys: Sys,
-//     timezone: i32,
-//     id: i32,
-//     name: String,
-//     cod: i32
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Forecast {
+    coord: Coord,
+    weather: Weather,
+    base: String,
+    main: Temps,
+    visibility: i32,
+    wind: Wind,
+    clouds: Clouds,
+    dt: i32,
+    sys: Sys,
+    timezone: i32,
+    id: i32,
+    name: String,
+    cod: i32
+}
 
-// #[derive(Serialize, Deserialize)]
-// struct Coord {
-//     lon: f32,
-//     lat: f32
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Coord {
+    lon: f32,
+    lat: f32
+}
 
-// #[derive(Serialize, Deserialize)]
-// struct Weather {
-//     id: i32,
-//     main: String,
-//     description: String,
-//     icon: String
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Weather {
+    details: Details,
+}
 
-// #[derive(Serialize, Deserialize)]
-// struct Temps {
-//     temp: f32,
-//     feels_like: f32,
-//     pressure: i32,
-//     humidity: i32,
-//     temp_min: f32,
-//     temp_max: f32,
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Details {
+    id: i32,
+    main: String,
+    description: String,
+    icon: String
+}
 
-// #[derive(Serialize, Deserialize)]
-// struct Wind {
-//     speed: f32,
-//     deg: f32,
-//     gust: f32,
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Temps {
+    temp: f32,
+    feels_like: f32,
+    pressure: i32,
+    humidity: i32,
+    temp_min: f32,
+    temp_max: f32,
+}
 
-// #[derive(Serialize, Deserialize)]
-// struct Clouds {
-//     all: i32
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Wind {
+    speed: f32,
+    deg: f32,
+}
 
-// #[derive(Serialize, Deserialize)]
-// struct Sys {
-//     type_: i32,
-//     id: i32,
-//     country: String,
-//     sunrise: i32,
-//     sunset: i32
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Clouds {
+    all: i32
+}
 
-// impl Forecast {
-//     async fn get( city: &String, country_code: &String ) -> Result<Self, ExitFailure> {
-//         let url: String = format!("https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}", city, country_code, WEATHER_API_KEY);
-//         let url: Url = Url::parse(&*url)?; // ? propogarses the error
+#[derive(Serialize, Deserialize, Debug)]
+struct Sys {
+    r#type: i32,
+    id: i32,
+    country: String,
+    sunrise: i32,
+    sunset: i32
+}
 
-//         let resp = reqwest::get(url).await?.json::<Forecast>().await?;// ? propogarses the error
+impl Forecast {
+    async fn get( city: &String, country_code: &String ) -> Result<Self, ExitFailure> {
+        let url: String = format!("https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}", city, country_code, WEATHER_API_KEY);
+        let url: Url = Url::parse(&*url)?; // ? propogarses the error
 
-//         Ok(resp)
+        let resp = reqwest::get(url).await?.json::<Forecast>().await?;// ? propogarses the error
 
-//     }
-// }
+        Ok(resp)
+
+    }
+}
 
 #[tokio::main] // async main function
 async fn main() -> Result<(), ExitFailure> {
     let args = CLI::from_args();
-    // let response = Forecast::get(&args.city, &args.country_code).await?;
-    let req: String = format!("https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}", args.city, args.country_code, WEATHER_API_KEY);
-    let url: Url = Url::parse(&*req)?;
-    let response = reqwest::get(url).await?;
+    let response = Forecast::get(&args.city, &args.country_code).await?;
 
-    // println!("City: {} \nCountry Code: {} \nWeather: {}", args.city, args.country_code, response.weather.main);
-    println!("{}", response.text().await?);
+    println!("City: {} \nCountry Code: {} \nWeather: {}", args.city, args.country_code, response.weather.details.main);
     Ok(())
 }
+
+// #[tokio::main] // async main function
+// async fn main() -> Result<(), ExitFailure> {
+//     let args = CLI::from_args();
+//     // let response = Forecast::get(&args.city, &args.country_code).await?;
+//     let req: String = format!("https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}", args.city, args.country_code, WEATHER_API_KEY);
+//     let url: Url = Url::parse(&*req)?;
+//     let response = reqwest::get(url).await?;
+
+//     // println!("City: {} \nCountry Code: {} \nWeather: {}", args.city, args.country_code, response.weather.main);
+//     println!("{}", response.text().await?);
+//     Ok(())
+// }
+
+
 
