8f08bbf0a9c97ca032de3a46548d3b66e27cc585
m6keller <kellermatthewe@gmail.com>
2022-06-13 18:47:36 -0700

    module system

---
+++ src/lib/types.rs
@@ -0,0 +1,92 @@
+
+
+use structopt::StructOpt; // automatically parses the arguments
+use serde_derive::{Deserialize, Serialize}; // deserialize the json file
+use reqwest::Url; // url parsing   
+use exitfailure::{ExitFailure}; // error handling
+
+mod hidden;
+
+#[derive(StructOpt)] // handles/parses CLI arguments
+pub struct CLI {
+    pub city: String,
+    pub country_code: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Forecast {
+    pub coord: Coord,
+    pub weather: Weather,
+    pub base: String,
+    pub main: Temps,
+    pub visibility: i32,
+    pub wind: Wind,
+    pub clouds: Clouds,
+    pub dt: i32,
+    pub sys: Sys,
+    pub timezone: i32,
+    pub id: i32,
+    pub name: String,
+    pub cod: i32
+}
+
+
+impl Forecast {
+    pub async fn get( city: &String, country_code: &String ) -> Result<Self, ExitFailure> {
+        let url: String = format!("https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}", city, country_code, crate::lib::types::hidden::WEATHER_API_KEY);
+        let url: Url = Url::parse(&*url)?; // ? propogarses the error
+
+        let resp = reqwest::get(url).await?.json::<Forecast>().await?;// ? propogarses the error
+
+        Ok(resp)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Coord {
+    pub lon: f32,
+    pub lat: f32
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Weather {
+    pub details: Details,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Details {
+    pub id: i32,
+    pub main: String,
+    pub description: String,
+    pub icon: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Temps {
+    pub temp: f32,
+    pub feels_like: f32,
+    pub pressure: i32,
+    pub humidity: i32,
+    pub temp_min: f32,
+    pub temp_max: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wind {
+    pub speed: f32,
+    pub deg: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Clouds {
+    pub all: i32
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Sys {
+    pub r#type: i32,
+    pub id: i32,
+    pub country: String,
+    pub sunrise: i32,
+    pub sunset: i32
+}
--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,11 @@
 use structopt::StructOpt; // automatically parses the arguments
 use exitfailure::{ExitFailure}; // error handling
-use serde_derive::{Deserialize, Serialize}; // deserialize the json file
-use reqwest::Url; // url parsing   
 
-// use openweathermap.org/api
-
-const WEATHER_API_KEY: &str = "c990fe48e97da433565f1f9e5bd1c560";
-
-#[derive(StructOpt)] // handles/parses CLI arguments
-struct CLI {
-    city: String,
-    country_code: String
+mod lib {
+    pub mod types;
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Forecast {
-    coord: Coord,
-    weather: Weather,
-    base: String,
-    main: Temps,
-    visibility: i32,
-    wind: Wind,
-    clouds: Clouds,
-    dt: i32,
-    sys: Sys,
-    timezone: i32,
-    id: i32,
-    name: String,
-    cod: i32
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Coord {
-    lon: f32,
-    lat: f32
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Weather {
-    details: Details,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Details {
-    id: i32,
-    main: String,
-    description: String,
-    icon: String
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Temps {
-    temp: f32,
-    feels_like: f32,
-    pressure: i32,
-    humidity: i32,
-    temp_min: f32,
-    temp_max: f32,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Wind {
-    speed: f32,
-    deg: f32,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Clouds {
-    all: i32
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Sys {
-    r#type: i32,
-    id: i32,
-    country: String,
-    sunrise: i32,
-    sunset: i32
-}
-
-impl Forecast {
-    async fn get( city: &String, country_code: &String ) -> Result<Self, ExitFailure> {
-        let url: String = format!("https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}", city, country_code, WEATHER_API_KEY);
-        let url: Url = Url::parse(&*url)?; // ? propogarses the error
-
-        let resp = reqwest::get(url).await?.json::<Forecast>().await?;// ? propogarses the error
-
-        Ok(resp)
-
-    }
-}
+use crate::lib::types::{Forecast, CLI};
 
 #[tokio::main] // async main function
 async fn main() -> Result<(), ExitFailure> {
@@ -99,19 +15,3 @@ async fn main() -> Result<(), ExitFailure> {
     println!("City: {} \nCountry Code: {} \nWeather: {}", args.city, args.country_code, response.weather.details.main);
     Ok(())
 }
-
-// #[tokio::main] // async main function
-// async fn main() -> Result<(), ExitFailure> {
-//     let args = CLI::from_args();
-//     // let response = Forecast::get(&args.city, &args.country_code).await?;
-//     let req: String = format!("https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}", args.city, args.country_code, WEATHER_API_KEY);
-//     let url: Url = Url::parse(&*req)?;
-//     let response = reqwest::get(url).await?;
-
-//     // println!("City: {} \nCountry Code: {} \nWeather: {}", args.city, args.country_code, response.weather.main);
-//     println!("{}", response.text().await?);
-//     Ok(())
-// }
-
-
-
